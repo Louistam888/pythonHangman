213cc9408065606f01d2be809e3f9c4321318d4f
Louistam888 <louistam888@gmail.com>
2023-04-02 23:40:28 -0400

    finished victory condition for loop

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 display = []
-guess = input("Guess a letter: ").lower()
 
 for letter in chosen_word: 
   display.extend("_")
 
-for i in range(0, len(chosen_word)):
-  if chosen_word[i] == guess:
-    display[i] = guess
+game_over = False
 
-print(display)
+while game_over == False: 
+  guess = input("Guess a letter: ").lower()
+
+  for i in range(0, word_length):
+    if chosen_word[i] == guess:
+      display[i] = guess
+
+  print(display)
+
+  if "_" not in display: 
+    game_over = True
+
+print("You win!")
