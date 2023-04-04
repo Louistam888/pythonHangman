8c2467c0d3fc8ce0d9d0eeef223b9ebbc77a418d
Louistam888 <louistam888@gmail.com>
2023-04-03 22:14:22 -0400

    completed basic logic

--- main.py
+++ main.py
@@ -1,7 +1,66 @@
 import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+lives = 6
 
 display = []
 
@@ -13,13 +72,28 @@ game_over = False
 while game_over == False: 
   guess = input("Guess a letter: ").lower()
 
+  guess_successful = False
+  
   for i in range(0, word_length):
     if chosen_word[i] == guess:
       display[i] = guess
+      guess_successful = True
 
-  print(display)
+  if guess_successful == False:
+    lives -= 1
+    
+    if lives != 0:
+      print(f"Guess again! You have {lives} lives left \n")
+    elif lives == 0: 
+      print("You lose!")
+      print(f"The answer was {chosen_word}")
+      game_over = True
+  elif guess_successful == True: 
+    print("Good guess!")
+    
+  print(stages[lives])
+  print(f"{display} \n")
 
   if "_" not in display: 
     game_over = True
-
-print("You win!")
+    print("You win!")
